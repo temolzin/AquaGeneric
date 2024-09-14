b6588797a7d70ed53c89a751d1a4224c47a4a8c9
josejulianmtnez <martinez.delacruzjosejulian32@gmail.com>
2024-09-13 23:37:00 -0600

    fix/identation-in-localities-create-view

--- resources/views/localities/create.blade.php
+++ resources/views/localities/create.blade.php
@@ -73,12 +73,14 @@
 
 <script>
     function previewImage(event) {
-    var reader = new FileReader();
-    reader.onload = function(){
-        var output = document.getElementById('photo-preview');
-        output.src = reader.result;
-        output.style.display = 'block';
+        var reader = new FileReader();
+
+        reader.onload = function() {
+            var output = document.getElementById('photo-preview');
+            output.src = reader.result;
+            output.style.display = 'block';
+        }
+
+        reader.readAsDataURL(event.target.files[0]);
     }
-    reader.readAsDataURL(event.target.files[0]);
-}
 </script>
