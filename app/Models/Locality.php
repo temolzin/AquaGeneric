0c9d089492230ff0021ea67a95410fbacf5f5d80
josejulianmtnez <martinez.delacruzjosejulian32@gmail.com>
2024-09-17 18:29:20 -0600

    fix/add-soft-deletes-to-localities-table

--- app/Models/Locality.php
+++ app/Models/Locality.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Image\Enums\Fit;
@@ -11,8 +12,8 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Locality extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
-    
+    use HasFactory, InteractsWithMedia, SoftDeletes;
+
     protected $fillable = [
         'locality_name',
         'municipality',
