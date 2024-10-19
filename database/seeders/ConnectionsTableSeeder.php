7c963e972be481937a2b6d90a39773c8dfb679c8
And-Y21 <andychevin21@gmail.com>
2024-10-18 18:05:32 -0600

    Add Connection model and migration

---
+++ database/seeders/ConnectionsTableSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use App\Models\Connection;
+
+class ConnectionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        $customers = DB::table('customers')->pluck('id');
+        $localities = DB::table('localities')->pluck('id');
+        $costs = DB::table('costs')->pluck('id');
+        $users = DB::table('users')->pluck('id');
+
+        foreach ($customers as $customerId) {
+            Connection::create([
+                'customer_id' => $customerId,
+                'locality_id' => $faker->randomElement($localities),
+                'cost_id' => $faker->randomElement($costs),
+                'created_by' => $faker->randomElement($users),
+                'name' => $faker->streetName,
+                'occupants_number' => $faker->numberBetween(1, 10),
+                'water_days' => $faker->numberBetween(1, 7),
+                'has_water_pressure' => $faker->boolean,
+                'has_cistern' => $faker->boolean,
+                'type' => $faker->randomElement(['residencial', 'commercial']),
+            ]);
+
+            if ($faker->boolean(30)) {
+                Connection::create([
+                    'customer_id' => $customerId,
+                    'locality_id' => $faker->randomElement($localities),
+                    'cost_id' => $faker->randomElement($costs),
+                    'created_by' => $faker->randomElement($users),
+                    'name' => $faker->streetName,
+                    'occupants_number' => $faker->numberBetween(1, 10),
+                    'water_days' => $faker->numberBetween(1, 7),
+                    'has_water_pressure' => $faker->boolean,
+                    'has_cistern' => $faker->boolean,
+                    'type' => $faker->randomElement(['residencial', 'commercial']),
+                ]);
+            }
+        }
+    }
+}
