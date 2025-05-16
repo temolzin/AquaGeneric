5ac4c7cb338a5d5b944fba7315edf0c7aaae2678
Daniel González García <gondshaggi@gmail.com>
2025-05-16 17:08:54 -0600

    Modify label costumers

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -57,7 +57,7 @@
                                 </div>
                                 <div class="col-lg-6">
                                     <div class="form-group">
-                                        <label>Estado</label>
+                                        <label>Estatus</label>
                                         <input type="text" disabled class="form-control" value="{{ $customer->state ?? 'Desconocido' }}" />
                                     </div>
                                 </div>
