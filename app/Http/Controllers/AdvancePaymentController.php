cde71927c25db76633c4d0e8e3872a6426a083e6
Daniel González García <gondshaggi@gmail.com>
2025-06-12 12:02:04 -0600

    Controller Correction

--- app/Http/Controllers/AdvancePaymentController.php
+++ app/Http/Controllers/AdvancePaymentController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{Payment, Customer};
+use App\Models\Payment;
+use App\Models\Customer;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Str;
 use Carbon\Carbon;
-use App\Models\Payment; 
 use App\Models\Debt;
 use Illuminate\Support\Facades\Log;
 
