e3c77904e6d14edfd7b31f8a5a1e6ecb104e784f
Daniel González García <gondshaggi@gmail.com>
2025-07-02 17:10:26 -0600

    Removing the search bar and the non-functional button

--- resources/views/advancePayments/index.blade.php
+++ resources/views/advancePayments/index.blade.php
@@ -20,27 +20,8 @@
                     data-target="#generateAdvancePaymentsReportModal">
                     <i class="fas fa-fw fa-calendar-plus"></i> Pagos Adelantados
                 </button>
-                <button class="btn btn-secondary mr-2" data-toggle="modal" data-target="#paymentChart">
-                    <i class="fa fa-dollar-sign"></i> Comprobante de pagos
-                </button>
             </div>
         </div>
-    </div>
-
-    <div class="col-lg-4 mt-3">
-        <form method="GET" action="" class="my-3">
-            <div class="input-group">
-                <input
-                    type="text"
-                    name="search"
-                    class="form-control"
-                    placeholder="Buscar por nombre o apellido"
-                    value="{{ request('search') }}">
-                <div class="input-group-append">
-                    <button type="submit" class="btn btn-primary">Buscar</button>
-                </div>
-            </div>
-        </form>
     </div>
 
     @include('advancePayments.paymentHistoryModal')
