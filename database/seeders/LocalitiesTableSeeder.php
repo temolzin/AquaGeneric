9652f12bf95a88a24b04feffb7c6d12e44177686
Neythhan <leonardo.n.gomez.h@gmail.com>
2025-07-23 18:29:08 -0600

    Generate and assign tokens to all localities

--- database/seeders/LocalitiesTableSeeder.php
+++ database/seeders/LocalitiesTableSeeder.php
@@ -4,35 +4,65 @@ namespace Database\Seeders;
 
 use App\Models\Locality;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
 
 class LocalitiesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        Locality::create([
-            'name' => 'Smallville',
-            'municipality' => 'Smallville',
-            'state' => 'Kansas',
-            'zip_code' => '66002',
-        ]);
-        
-        Locality::create([
-            'name' => 'Springfield',
-            'municipality' => 'Springfield',
-            'state' => 'Oregon',
-            'zip_code' => '97477',
-        ]);
+        $existingLocalities = Locality::whereNull('token')->get();
 
-        Locality::create([
-            'name' => 'Dunder Mifflin',
-            'municipality' => 'Scranton',
-            'state' => 'Pennsylvania',
-            'zip_code' => '18503',
+        foreach ($existingLocalities as $locality) {
+            $token = $this->getgenerateLocalityToken($locality->id);
+            $locality->token = $token;
+            $locality->save();
+        }
+
+        $localities = [
+            [
+                'name' => 'Smallville',
+                'municipality' => 'Smallville',
+                'state' => 'Kansas',
+                'zip_code' => '66002',
+            ],
+            [
+                'name' => 'Springfield',
+                'municipality' => 'Springfield',
+                'state' => 'Oregon',
+                'zip_code' => '97477',
+            ],
+            [
+                'name' => 'Dunder Mifflin',
+                'municipality' => 'Scranton',
+                'state' => 'Pennsylvania',
+                'zip_code' => '18503',
+            ],
+        ];
+
+        foreach ($localities as $data) {
+            $locality = Locality::create($data);
+            $token = $this->getgenerateLocalityToken($locality->id);
+            $locality->token = $token;
+            $locality->save();
+        }
+    }
+
+    private function getgenerateLocalityToken(int $id): string
+    {
+        $startDate = now()->format('Y-m-d');
+        $endDate = now()->addYear()->format('Y-m-d');
+
+        $data = [
+            'idLocality' => $id,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+        ];
+
+        $hmacSignature = hash_hmac('sha256', json_encode($data), env('TOKEN_SECRET_KEY'));
+
+        return Crypt::encrypt([
+            'data' => $data,
+            'hmac' => $hmacSignature,
         ]);
     }
 }
