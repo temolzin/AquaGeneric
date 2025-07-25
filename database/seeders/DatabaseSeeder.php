740410180e74a581f6fbe97fdc3b52b1972c181f
Neythhan <leonardo.n.gomez.h@gmail.com>
2025-07-25 15:44:52 -0600

    Seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,6 @@ class DatabaseSeeder extends Seeder
         $this->call(IncidentCategorySeeder::class);
         $this->call(IncidentSeeder::class);
         $this->call(EmployeesTableSeeder::class);
+        $this->call(LocalitiesTableSeeder::class);
     }
 }
