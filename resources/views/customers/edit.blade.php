66cb74961a2f949c7cb42be1a55ab80a20228dab
SharolRI <sriveronislas@gmail.com>
2025-09-18 15:16:43 -0600

    fix: removed make_exterior_number_nullable_in_customers_table.php file and edited tags to accept NULL values

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -83,7 +83,7 @@
                                     </div>
                                     <div class="col-lg-4">
                                         <div class="form-group">
-                                            <label for="exteriorNumberUpdate" class="form-label">Manzana(*)</label>
+                                            <label for="exteriorNumberUpdate" class="form-label">Manzana</label>
                                             <input type="text" class="form-control" name="exteriorNumberUpdate" id="exteriorNumberUpdate" value="{{ $customer->exterior_number }}" >
                                         </div>
                                     </div>
