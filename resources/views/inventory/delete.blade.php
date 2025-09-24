96083bf7227dd6a46cbe764d72a33cd41db0fa00
EduardoQC <quelitechanes@gmail.com>
2025-09-24 14:20:49 -0600

    variable correction
    The variables that were in Spanish were corrected to English.

--- resources/views/inventory/delete.blade.php
+++ resources/views/inventory/delete.blade.php
@@ -1,17 +1,17 @@
-<div class="modal fade" id="delete{{ $componente->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteInventoryLabel{{ $componente->id }}" aria-hidden="true">
+<div class="modal fade" id="delete{{ $component->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteInventoryLabel{{ $component->id }}" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-danger">
-                <h5 class="modal-title" id="deleteInventoryLabel{{ $componente->id }}">Eliminar Componente</h5>
+                <h5 class="modal-title" id="deleteInventoryLabel{{ $component->id }}">Eliminar Componente</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="{{ route('inventory.destroy', $componente->id) }}" method="post">
+            <form action="{{ route('inventory.destroy', $component->id) }}" method="post">
                 @csrf
                 @method('DELETE')
                 <div class="modal-body text-center text-danger">
-                    ¿Estás seguro de eliminar el Componente <strong>{{ $componente->name }}</strong>?
+                    ¿Estás seguro de eliminar el Componente <strong>{{ $component->name }}</strong>?
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
