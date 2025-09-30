02edf8d262f74baf375d6d3d87e5082f4f6e6d7b
IsaacFM <isasifromez@gmail.com>
2025-09-30 17:54:24 -0600

    Remove space from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -193,15 +193,15 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h3 class="card-title">Ingresos Anuales por Mes<span id="localityInfoAnnual"></h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <canvas id="annualEarningsChart" width="400" height="200"></canvas>
-                                    </div>
+                    <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Ingresos Anuales por Mes<span id="localityInfoAnnual"></h3>
                                 </div>
+                                <div class="card-body">
+                                    <canvas id="annualEarningsChart" width="400" height="200"></canvas>
+                                </div>
+                            </div>
                             </div>
                         </div>
                     @endcan
