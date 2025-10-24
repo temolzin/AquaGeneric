cbbf52db66a2256682fb84a5431617c52aa6e70b
AdrianRA <pechuga564yty@gmail.com>
2025-10-24 11:50:00 -0600

    Modify name of function generatePDF to generateUserAccessPDF

--- routes/web.php
+++ routes/web.php
@@ -82,7 +82,7 @@ Route::group(['middleware' => ['auth', CheckSubscription::class]], function () {
         Route::get('/report/pdfCustomersSummary', [CustomerController::class, 'generateCustomerSummaryPdf'])->name('customers.pdfCustomersSummary');
         Route::get('/report/current-customers', [CustomerController::class, 'reportCurrentCustomers'])->name('report.current-customers');
         Route::get('/payment-history/{id}', [CustomerController::class, 'generatePaymentHistoryReport'])->name('reports.paymentHistoryReport');
-        Route::get('/generate-customer-pdf/{id}', [CustomerController::class, 'generatePDF'])->name('customers.generatePDF');
+        Route::get('/generate-user-access-pdf/{id}', [CustomerController::class, 'generateUserAccessPDF'])->name('generate.user.access.pdf');
     });
 
     Route::group(['middleware' => ['can:viewRoles']], function () {
