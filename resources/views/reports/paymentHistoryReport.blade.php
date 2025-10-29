58591d5e9c861e20bfd507e054d27f1391483e82
Paulina Plaza <paulinalilianpc@gmail.com>
2025-10-28 18:13:57 -0600

    update default horizontal background image in reports

--- resources/views/reports/paymentHistoryReport.blade.php
+++ resources/views/reports/paymentHistoryReport.blade.php
@@ -7,7 +7,7 @@
 
     $horizontalBgPath = $locality && $locality->getFirstMedia('pdfBackgroundHorizontal')
         ? $locality->getFirstMedia('pdfBackgroundHorizontal')->getPath()
-        : public_path('img/customersBackground.png');
+        : public_path('img/customersBackgroundHorizontal.png');
 @endphp
 <!DOCTYPE html>
 <html lang="es">
