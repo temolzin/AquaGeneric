5f478cce4856dd839561897127a74c7e39e43473
AdrianRA <pechuga564yty@gmail.com>
2025-11-27 15:19:21 -0600

    Color Preview Sync, Badge Styling, and Customer Selection Update

--- resources/views/waterConnections/edit.blade.php
+++ resources/views/waterConnections/edit.blade.php
@@ -39,9 +39,9 @@
                                         <select class="form-control select2" name="customerIdUpdate" id="customerIdUpdate" required>
                                             <option value=""></option>
                                                 @foreach($customers as $customer)
-                                                    @if($customer->user)
+                                                    @if($customer)
                                                         <option value="{{ $customer->id }}" {{ $customer->id == $connection->customer_id ? 'selected' : '' }}>
-                                                            {{ $customer->user->id }} - {{ $customer->user->name }} {{ $customer->user->last_name }}
+                                                            {{ $customer->id }} - {{ $customer->name }} {{ $customer->last_name }}
                                                         </option>
                                                     @endif
                                                 @endforeach
