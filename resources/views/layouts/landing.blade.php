9273f2f1c28a7b365d3fc9165339fbeb730512ab
Humberto-Lopez <168702715+Humberto-Lopez@users.noreply.github.com>
2026-01-28 19:34:20 -0600

    fix: Remove unused revolution directory

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -11,15 +11,13 @@
     <link rel="stylesheet" href="{{ asset('assets/index/css/jquery.mCustomScrollbar.min.css') }}">
     @stack('styles')
 </head>
-<body class="main-layout"> 
+<body class="main-layout">
     <div class="loader_bg">
         <div class="loader">
             <img src="{{ asset('img/loading_waterdrop.gif') }}" alt="">
         </div>
     </div>
-
     @yield('content')
-
     <script src="{{ asset('assets/index/js/jquery.min.js') }}"></script>
     <script src="{{ asset('assets/index/js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/index/js/bootstrap.bundle.min.js') }}"></script>
