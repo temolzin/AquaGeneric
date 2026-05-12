fab2517bf60fa42873510d67fe4eb1bed249b398
Alexis1701 <alexis.yair.hernandez17@gmail.com>
2026-05-12 13:22:47 -0600

    Fix add membership button visibility

--- resources/views/memberships/index.blade.php
+++ resources/views/memberships/index.blade.php
@@ -24,6 +24,20 @@
                                         </div>
                                     </form>
                                     <div class="d-flex flex-wrap justify-content-end gap-2 w-100 w-md-auto">
+
+                                        @can('createMemberships')
+                                            <button type="button"
+                                                    class="btn btn-success"
+                                                    data-toggle="modal"
+                                                    data-target="#createMembership"
+                                                    style="white-space: nowrap;">
+
+                                                <i class="fas fa-plus"></i>
+                                                Agregar Membresía
+
+                                            </button>
+                                        @endcan
+
                                     </div>
                                 </div>
                             </div>
@@ -97,6 +111,7 @@
                                             @endif
                                         </tbody>
                                     </table>
+                                    @include('memberships.create')
                                     <div class="d-flex justify-content-center">
                                         {!! $memberships->links('pagination::bootstrap-4') !!}
                                     </div>
