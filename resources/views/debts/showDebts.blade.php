046bd7bf93b0d534392b1b9af3e51b0a24428d28
Alexis1701 <alexis.yair.hernandez17@gmail.com>
2026-05-15 16:53:18 -0600

    Add space to the table and show all tables in the report list

--- resources/views/debts/showDebts.blade.php
+++ resources/views/debts/showDebts.blade.php
@@ -163,7 +163,8 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
                 </div>
             </div>
         </div>
