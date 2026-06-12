8433adf81697e656a5e714e48d8280e0410c0b71
Alexis1701 <alexis.yair.hernandez17@gmail.com>
2026-06-12 15:18:42 -0600

    Correct header layout and button alignment

--- resources/views/localities/index.blade.php
+++ resources/views/localities/index.blade.php
@@ -25,19 +25,19 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="col-md-7 text-md-right mt-2 mt-md-0">
-                            <div class="btn-group" role="group"aria-label="Acciones de Usuario">
-                                <button class="btn btn-success mr-2" data-toggle='modal' data-target="#createLocality">
+                            <div class="col-md-7 text-right">
+                                <button class="btn btn-success mr-2" data-toggle="modal" data-target="#createLocality">
                                     <i class="fa fa-plus"></i> Registrar Localidad
                                 </button>
-                                <a type="button" class="btn btn-secondary mr-2" target="_blank" title="Localities" href="#">
+
+                                <a class="btn btn-secondary mr-2" target="_blank" href="{{ route('localities.pdfLocalities', ['search' => request('search')]) }}">
                                     <i class="fas fa-map"></i> Generar Lista
                                 </a>
-                                <a type="button" class="btn btn-info" href="{{ asset('docs/GUIA_PARA_REGISTRAR.pdf') }}" target="_blank" title="Ver guía de registro">
+
+                                <a class="btn btn-info" href="{{ asset('docs/GUIA_PARA_REGISTRAR.pdf') }}" target="_blank">
                                     <i class="fas fa-file-pdf"></i> Guía OpenPay
                                 </a>
                             </div>
-                        </div>
                     </div>
                     <div class="clearfix"></div>
                 </div>
