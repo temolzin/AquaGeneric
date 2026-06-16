8db3c6180d7733c25819a2e7024c56c4e8b5fe94
Marco <soloparausodecursos@gmail.com>
2026-06-16 11:20:53 -0600

    design height and resizing of the membership info box

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -24,9 +24,9 @@
     @endphp
     <section class="content">
         <div class="container-fluid">
-            <div class="row">
+            <div class="row d-flex align-items-stretch">
                 <div class="col-md-3">
-                    <div class="card card-primary">
+                    <div class="card card-primary h-100 mb-0">
                         <div class="card-header">
                             <h3 class="card-title">Foto de perfil</h3>
                         </div>
@@ -50,7 +50,7 @@
                     </div>
                 </div>
                 <div class="col-md-9">
-                    <div class="card card-primary">
+                    <div class="card card-primary h-100 mb-0">
                         <div class="card-header">
                             <h3 class="card-title">Editar perfil</h3>
                         </div>
@@ -150,8 +150,12 @@
                             </div>
                         </div>
                     </div>
-                    @can('viewDashboardCards')
-                    <div class="col-md-12">
+                </div>
+            </div>
+                        
+            @can('viewDashboardCards')
+            <div class="row mt-3">
+                <div class="col-md-12">
                         <div class="card card-info">
                             <div class="card-header">
                                 <h3 class="card-title">Información de Membresía</h3>
@@ -266,9 +270,8 @@
                             </div>
                         </div>
                     </div>
-                    @endcan
-                </div>
             </div>
+            @endcan
         </div>
     </section>
     @include('profile.editImage')
