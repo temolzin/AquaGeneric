b8eca4a0f687353212e1276f4c006adaf8761b1b
Marco <soloparausodecursos@gmail.com>
2026-07-09 11:54:07 -0600

    Responsive design adjustment applied to debts module interface for better layout across devices

--- resources/views/debts/index.blade.php
+++ resources/views/debts/index.blade.php
@@ -27,8 +27,7 @@
                                         <i class="fa fa-plus"></i>
                                         Asignar a Todos
                                     </button>
-                                    <button type="button" class="btn btn-success mx-1" data-toggle="modal"
-                                            title="Registrar Deuda" data-target="#createDebt">
+                                    <button type="button" class="btn btn-success mb-2 mb-lg-0 mx-0 mx-lg-1 w-100 w-lg-auto" data-toggle="modal" title="Registrar Deuda" data-target="#createDebt">
                                         <i class="fa fa-plus"></i>
                                         <span class="d-none d-lg-inline">Registrar Deuda</span>
                                         <span class="d-inline d-lg-none">Registrar Deuda</span>
